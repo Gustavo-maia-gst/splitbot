ca855511ec0a6de0d9a4643a753250fe89fb7502
Gustavo <gustavo.gst15@gmail.com>
2026-02-14 11:10:43 -0300

    feat: add github

--- src/mcp/specializations/index.ts
+++ src/mcp/specializations/index.ts
@@ -2,5 +2,16 @@ import { GetLogContextTool } from './gcp/logs_explorer/get_log_context/get-log-c
 import { ListLogsTool } from './gcp/logs_explorer/list_logs/list-logs.tool';
 import { ListSqlInstancesTool } from './gcp/database_status/list_sql_instances/list-sql-instances.tool';
 import { DetailSqlInstanceTool } from './gcp/database_status/detail_sql_instance/detail-sql-instance.tool';
+import { ListRepositoriesTool } from './github/list_repositories/list-repositories.tool';
+import { ListCommitsTool } from './github/list_commits/list-commits.tool';
+import { ListReleasesTool } from './github/list_releases/list-releases.tool';
 
-export const TOOLS = [ListLogsTool, GetLogContextTool, ListSqlInstancesTool, DetailSqlInstanceTool];
+export const TOOLS = [
+  ListLogsTool,
+  GetLogContextTool,
+  ListSqlInstancesTool,
+  DetailSqlInstanceTool,
+  ListRepositoriesTool,
+  ListCommitsTool,
+  ListReleasesTool,
+];
